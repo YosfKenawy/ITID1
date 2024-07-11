db5dcfd5a6656d0bb39a3c6ea3117571b8711141
AIT <AIT@DESKTOP-87BTOD1>
2024-07-11 14:37:18 +0300

    Add project files.

---
+++ ITID1/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ITID1
+{
+    class student
+    {
+        int age;
+        int id;
+        string phone;
+        string name;
+        
+
+        public void print()
+        {
+            Console.WriteLine($"{id} - {name} {age} - {phone}");
+        }
+        static void Main(string[] args)
+        {
+            student student = new student();
+            student.print();
+          
+
+
+
+            
+
+           
+            
+        }
+    }
+
+    
+    
+
+
+}
