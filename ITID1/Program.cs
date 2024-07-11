f36a01eb44dabe52cc1b5394a47562c84a840a85
yousef kenawy <yosefkeno3100@gmail.com>
2024-07-11 21:26:35 +0300

    Lab C# day 2

--- ITID1/Program.cs
+++ ITID1/Program.cs
@@ -1,5 +1,8 @@
-﻿namespace ITID1
+﻿using System.Transactions;
+
+namespace ITID1
 {
+    #region
     class student
     {
         int age;
@@ -16,6 +19,10 @@
         {
             student student = new student();
             student.print();
+
+
+
+
           
 
 
@@ -26,9 +33,85 @@
             
         }
     }
+    #endregion
+    #region
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Enter the number : ");
+            int num = int . Parse( Console.ReadLine() );
 
-    
-    
+            for (int i = 0; i <+12; i++)
+            {
+
+
+                Console.Write($" {num} x {i} = {num * i}");
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+    #endregion
+    #region
+    internal class program1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your id : ");
+            int id = int . Parse( Console.ReadLine() );
+
+            Console.Write("Enter your age : ");
+            int age;
+
+            do
+            {
+                age = int . Parse( Console.ReadLine() );
+            }while ( age < 1 );
+
+            Console.WriteLine
+                ($@"
+                Name : {name}
+                ID : {id}
+                age : {age}
+                "
+                );
+
+
+
+
+
+        }
+
+
+
+    }
+    #endregion
+
+
+
+
 
 
 }
